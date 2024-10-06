48c969a29cc9a14584f6604f8453382d00d056f5
szmsu2011 <szmsu2011@hotmail.co.nz>
2024-10-06 20:37:22 +1100

    minor adjustments

--- report/report.R
+++ report/report.R
@@ -46,7 +46,6 @@ bind_rows(
   geom_line(aes(x, f, col = type)) +
   geom_point(aes(x, 0), tibble(x = test$lSalary), shape = 1) +
   annotate("text", x = 5.93, y = 0, label = "Test data", vjust = -1) +
-  annotate("text", x = 5.93, y = 0.4, label = "Trained curves") +
   theme_bw() +
-  theme(legend.position = "bottom") +
-  labs(x = "log(Salary)", y = expr(hat(f)), col = "")
+  theme(legend.position = "top") +
+  labs(x = "log(Salary)", y = expr(hat(f)), col = "Trained curves")
