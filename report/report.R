eefa938c2693c7a349ca7a8a86ca6395e314c121
szmsu2011 <szmsu2011@hotmail.co.nz>
2024-10-06 20:30:57 +1100

    finish section 3

--- report/report.R
+++ report/report.R
@@ -40,11 +40,13 @@ bind_rows(
     f = dnorm(x, mean(train$lSalary), sd(train$lSalary))
   ),
   tibble(x = kd_rot$x, type = "KDE (ROT)", f = kd_rot$y),
-  tibble(x = kd_cv$x, type = "KDE (CV)", f = kd_cv$y)
+  tibble(x = kd_cv$x, type = "KDE (LSCV)", f = kd_cv$y)
 ) |>
   ggplot() +
   geom_line(aes(x, f, col = type)) +
   geom_point(aes(x, 0), tibble(x = test$lSalary), shape = 1) +
+  annotate("text", x = 5.93, y = 0, label = "Test data", vjust = -1) +
+  annotate("text", x = 5.93, y = 0.4, label = "Trained curves") +
   theme_bw() +
   theme(legend.position = "bottom") +
   labs(x = "log(Salary)", y = expr(hat(f)), col = "")
